90b62624d5806f3bba1611b28756141ea2460d1a
ChokoJoestar <mohamedboulidam680@gmail.com>
2023-04-29 20:18:27 +0200

    Creating a function that puts the logs in a file

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,37 +1,102 @@
 import chalk from "chalk";
 import dayjs from "dayjs";
 import { colorBgTag, colorTag } from "./types/color";
+import { appendFileSync, existsSync, mkdirSync } from "fs";
 
 const format = "{timestamp} - {tag} {msg}\n";
 
-export class Loggers {
+type LogParms = {
+   tagColor: colorTag;
+   bgTagColor: colorBgTag;
+   tag: string;
+   error: boolean;
+};
 
-   public error(content: string): void | string {
-      return this.write(content, "black", "bgWhite", " ⛔ ", true);
+export class Loggers {
+   public warn(content: string): void {
+      const parms: LogParms = {
+         tagColor: "whiteBright",
+         bgTagColor: "bgYellowBright",
+         tag: "WARN",
+         error: false,
+      };
+
+      this.writeToConsole(content, parms);
+      this.writeToFile(content, parms.tag);
    }
-   public warn(content: string): void | string {
-      return this.write(content, "black", "bgWhite", " ⚠️  ", false);
+   public error(content: string): void {
+      const parms: LogParms = {
+         tagColor: "black",
+         bgTagColor: "bgRedBright",
+         tag: "ERROR",
+         error: true,
+      };
+
+      this.writeToConsole(content, parms);
+      this.writeToFile(content, parms.tag);
    }
-   public command(content: string): void | string {
-      return this.write(content, "black", "bgMagenta", " 🤖" , false);
+   public event(content: string): void {
+      const parms: LogParms = {
+         tagColor: "black",
+         bgTagColor: "bgGreen",
+         tag: "EVENT",
+         error: false,
+      };
+
+      this.writeToConsole(content, parms);
+      this.writeToFile(content, parms.tag);
    }
-   public event(content: string): void | string {
-      return this.write(content, "black", "bgGreen", " 🎉 ", false);
+   public client(content: string): void {
+      const parms: LogParms = {
+         tagColor: "black",
+         bgTagColor: "bgBlack",
+         tag: "CLIENT",
+         error: false,
+      };
+
+      this.writeToConsole(content, parms);
+      this.writeToFile(content, parms.tag);
    }
-   public client(content: string): void | string {
-      return this.write(content, "black", "bgBlack", " 💻 ", false);
-   }
-   
-   protected write(msg: string, tagColor: colorTag, bgTagColor: colorBgTag, tag: string, error: boolean = false): void{
-      const timestamp = `[${dayjs().format("DD/MM - HH:mm")}]`;
-      const logTag = `[${tag}]`;
-      const steam = error ? process.stderr : process.stdout;
+
+   private writeToConsole(
+      msg: string,
+      parms: {
+         tagColor: colorTag;
+         bgTagColor: colorBgTag;
+         tag: string;
+         error: boolean;
+      }
+   ): void {
+      const steam = parms.error ? process.stderr : process.stdout;
+      const timestamp = `[${dayjs().format("DD/MM - HH:mm")}]`
+      const tag = `[${parms.tag}]`;
 
       const item = format
          .replace("{timestamp}", chalk.gray(timestamp))
-         .replace("{tag}", chalk[bgTagColor][tagColor](`${logTag}`))
-         .replace("{msg}", chalk.white(msg));   
+         .replace(
+            "{tag}",
+            chalk[parms.bgTagColor][parms.tagColor](`${tag}`)
+         )
+         .replace("{msg}", chalk.white(msg));
 
       steam.write(item);
+   }
+
+   private writeToFile(content: string, tag: string) {
+      const logsDir: string = "./logs";
+      const timestamp = `[${dayjs().format("DD/MM - HH:mm")}]`
+      const getTag = `[${tag}]`;
+
+      if (!existsSync(logsDir)) {
+         mkdirSync(logsDir);
+      }
+
+      const logsContent = `${timestamp} - ${getTag}: ${content}`;
+
+      appendFileSync(`${logsDir}/lasted.log`, `${logsContent}\n`);
+   }
+
+   private getTag(tag: string): string {
+      return `[${tag}]`;
    }
 }
